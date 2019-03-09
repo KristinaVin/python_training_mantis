8f41a4ed4ea23b75118f25a985e48f3940ae5036
Kristina <ladykristy@mail.ru>
2019-03-09 21:20:46 +0600

    25 задание

--- fixture/application.py
+++ fixture/application.py
@@ -1,40 +1,22 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
-
+from fixture.project import ProjectHelper
 
 
 class Application:
 
-
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, baseURL):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
-        elif  browser == "chrome":
+        elif browser == "chrome":
             self.wd = webdriver.Chrome()
-        elif  browser == "ie":
+        elif browser == "ie":
             self.wd = webdriver.Ie()
         else:
-            raise ValueError("Unrecognized Browser %s" % browser)
-        self.wd.implicitly_wait(2)
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.base_url = baseURL
         self.session = SessionHelper(self)
-        self.base_url=base_url
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get(self.base_url)
-
-    def destroy(self):
-        self.wd.quit()
-
-    def return_to_home_page(self):
-        wd = self.wd
-        if not len(wd.find_elements_by_name("add")) > 0:
-            wd.find_element_by_link_text("home").click()
-
-    def open_group_page_by_id(self, id):
-        wd = self.wd
-        if not wd.current_url.endswith("/?group=%s" % id):
-            wd.get(self.base_url + "?group=%s" % id)
+        self.project = ProjectHelper(self)
 
     def is_valid(self):
         try:
@@ -42,3 +24,9 @@ class Application:
             return True
         except:
             return False
+
+    def open_home_page(self, wd):
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
